032600c815232468ad93fff2838fe50a9d211524
json <tong_haijun@htaonet.com>
2019-08-18 17:56:03 +0800

    项目优化 - 异常捕获 统一处理

--- src/main/java/com/imooc/exception/SellException.java
+++ src/main/java/com/imooc/exception/SellException.java
@@ -1,11 +1,13 @@
 package com.imooc.exception;
 
 import com.imooc.enums.ResultEnum;
+import lombok.Getter;
 
 /**
  * Created by TongHaiJun
  * 2019/7/31 23:44
  */
+@Getter
 public class SellException extends RuntimeException {
 
     private Integer code;
--- src/main/java/com/imooc/handler/SellExceptionHandler.java
+++ src/main/java/com/imooc/handler/SellExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.imooc.handler;
 
 import com.imooc.config.ProjectUrlConfig;
+import com.imooc.controller.vo.ResultVO;
+import com.imooc.exception.SellException;
 import com.imooc.exception.SellerAuthorizeException;
+import com.imooc.utils.ResultVOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -28,5 +32,15 @@ public class SellExceptionHandler {
                 .concat("?returnUrl=")
                 .concat(projectUrlConfig.getSell())
                 .concat("/sell/seller/login"));
+    }
+
+    /**
+     * 异常统一处理
+     */
+    @ExceptionHandler(value = SellException.class)
+    @ResponseBody
+    public ResultVO handlerSellerException(SellException e) {
+
+        return ResultVOUtil.error(e.getCode(), e.getMessage());
     }
 }
